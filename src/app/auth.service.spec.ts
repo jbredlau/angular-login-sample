debdbe5d19ba55c1b7e8e8635e561f3cb1135611
Jörg Bredlau <joerg@bredimac.local>
2017-06-26 20:56:31 +0200

    Refactorings, null-check implementiert

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -17,14 +17,20 @@ describe('AuthService', () => {
     expect(service).toBeTruthy();
   }));
 
+  it('login should return false if user undefined or null', inject([AuthService], (service: AuthService) => {
+    expect(service.login(undefined)).toBeFalsy();
+    expect(service.login(null)).toBeFalsy();
+  }));
+
   it('login should return false if user not exists', inject([AuthService], (service: AuthService) => {
     user = new User("notexists@test.com","");
     expect(service.login(user)).toBeFalsy();
   }));
 
   it('should return true if user credentials are ok', inject([AuthService], (service: AuthService) => {
+    user = new User('admin@test.com','adminpassword');
+    expect(service.login(user)).toBeTruthy();
     user = new User('user@test.com','userpassword');
-    
     expect(service.login(user)).toBeTruthy();
   }));
 
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -7,6 +7,8 @@ export class AuthService {
   constructor() { }
 
   login(user: User): boolean {
+    if (!user) { return false };
+
     let storedUser = this.findUserByEmail(user.email);
     if ( storedUser && this.checkPassword(user.email, storedUser.email)) {
       localStorage.setItem("user", user.toString());
@@ -15,31 +17,31 @@ export class AuthService {
     return false;
   }
 
-isLoggedIn(): boolean {
-  return this.getAuthenticatedUser() !== null;
-}
-
-/*
- * Checken ob user existiert. Wenn ja zurückgeben
- * Hier gemockt, kommt im echten Leben aus einem Backendservice 
- */
-private findUserByEmail(email: string): User {
-  // fakeusers for sample app
-  let users: Array<User> = [
-    new User('admin@test.com','adminpassword'),
-    new User('user@test.com','userpassword')
-  ];
-  let storedUser = users.find(u => u.email === email);
-  return storedUser || null;
-}
-
-private checkPassword(passwort: string, storedPassword: string): boolean {
-  return passwort === storedPassword;
-}
-
-private getAuthenticatedUser(): any {
-    return localStorage.getItem('user');
+  isLoggedIn(): boolean {
+    return this.getAuthenticatedUser() !== null;
   }
+
+  /*
+  * Checken ob user existiert. Wenn ja zurückgeben
+  * Hier gemockt, kommt im echten Leben aus einem Backendservice 
+  */
+  private findUserByEmail(email: string): User {
+    // fakeusers for sample app
+    // TODO fake user auslagern in BackendMockService
+    let users: Array<User> = [
+      new User('admin@test.com','adminpassword'),
+      new User('user@test.com','userpassword')
+    ];
+    return users.find(u => u.email === email) || null;
+  }
+
+  private checkPassword(passwort: string, storedPassword: string): boolean {
+    return passwort === storedPassword;
+  }
+
+  private getAuthenticatedUser(): any {
+      return localStorage.getItem('user');
+    }
 
 
 }
