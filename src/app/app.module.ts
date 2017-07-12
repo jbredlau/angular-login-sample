bd92a9f4622d96d06a7140acc4c9cf53f90fbda1
Jörg Bredlau <joerg@bredimac.local>
2017-07-12 17:47:02 +0200

    added route for about-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' }
 
 ];
